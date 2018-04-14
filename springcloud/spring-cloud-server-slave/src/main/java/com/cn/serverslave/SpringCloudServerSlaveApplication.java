d826631e888efa3bbac4def46c5057642fe2e9a0
lzf <kk_ziyi@126.com>
2018-04-14 11:10:28 +0800

    主从配置

---
+++ springcloud/spring-cloud-server-slave/src/main/java/com/cn/serverslave/SpringCloudServerSlaveApplication.java
@@ -0,0 +1,14 @@
+package com.cn.serverslave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringCloudServerSlaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudServerSlaveApplication.class, args);
+	}
+}
