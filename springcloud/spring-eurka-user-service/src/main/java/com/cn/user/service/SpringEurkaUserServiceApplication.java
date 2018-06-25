a0362506ccd8196dbbac0f01af2934c81e098482
liuzhenfei.225 <abc@123!>
2018-06-25 13:21:14 +0800

    整合Mytais和logback 输出sql

--- springcloud/spring-eurka-user-service/src/main/java/com/cn/user/service/SpringEurkaUserServiceApplication.java
+++ springcloud/spring-eurka-user-service/src/main/java/com/cn/user/service/SpringEurkaUserServiceApplication.java
@@ -1,10 +1,12 @@
 package com.cn.user.service;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@MapperScan("com.cn.user.service.mapper")
 @EnableEurekaClient
 public class SpringEurkaUserServiceApplication {
 
--- springcloud/spring-eurka-user-service/src/main/java/com/cn/user/service/service/impl/UserServiceImpl.java
+++ springcloud/spring-eurka-user-service/src/main/java/com/cn/user/service/service/impl/UserServiceImpl.java
@@ -1,23 +1,22 @@
 package com.cn.user.service.service.impl;
 
+import com.cn.user.service.mapper.UserMapper;
 import com.cn.user.service.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import javax.annotation.Resource;
 
 @Service("userService")
 public class UserServiceImpl implements UserService {
 
-   /* @Resource
-    private UserMapper uiserMapper;*/
+    @Autowired
+    private UserMapper userMapper;
 
     @Override
     public boolean selectUserByUserName(String userName, String passWord) {
-     /*   int count =  uiserMapper.selectUserByUserName(userName,passWord);
+        int count = userMapper.selectUserByUserName(userName, passWord);
         if(count > 0){
             return true;
         }
-        return false;*/
-       return true;
+        return false;
     }
 }
