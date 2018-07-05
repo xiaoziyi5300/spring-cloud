3057e970436fb671fb901b6ef79b1db5388dfa34
xiaoziyi5300 <ilcold@126.com>
2018-07-05 17:22:38 +0800

    swgger

--- springcloud/spring-eurka-user-ui/src/main/java/com/cn/user/ui/SpringEurkaUserUiApplication.java
+++ springcloud/spring-eurka-user-ui/src/main/java/com/cn/user/ui/SpringEurkaUserUiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /***
  * EnableDiscoveryClient eurka 客户端
@@ -17,6 +18,7 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 @EnableFeignClients
 @EnableHystrixDashboard
 @EnableHystrix
+@EnableSwagger2
 public class SpringEurkaUserUiApplication {
 
 	public static void main(String[] args) {
