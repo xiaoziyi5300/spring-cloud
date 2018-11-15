a031fccbd4f996ee2e4dc17278f3652ede5e1c66
xiaoziyi5300 <ilcold@126.com>
2018-11-15 17:46:22 +0800

    springcloud conifg 整合 kafka bus 实现配置刷新

--- springcloud/spring-cloud-config-client/src/main/java/com/cn/configclient/controller/TestController.java
+++ springcloud/spring-cloud-config-client/src/main/java/com/cn/configclient/controller/TestController.java
@@ -2,6 +2,7 @@ package com.cn.configclient.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  * date 2018/4/16-9:53
  */
 @RestController
+@RefreshScope
 public class TestController {
 
     @Autowired
